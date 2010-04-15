61db1b36aaaf64f48aa650a93bf4a92a018c4a5c
Aki Reijonen <aki@multitouch.fi>
2010-04-15 17:34:42 +0300

    Interpolating (Hermite) spline to Nimble

--- multitude/Nimble/Splines.hpp
+++ multitude/Nimble/Splines.hpp
@@ -3,6 +3,8 @@
 
 #include <vector>
 
+#include <Nimble/Vector2.hpp>
+
 namespace Nimble
 {
 
@@ -18,6 +20,25 @@ namespace Nimble
         /// @return value of the spline at given t
         template<class T>
         T evalCatmullRom(float t, const std::vector<T> & cp, size_t index = 0);
+
+        /// Catmull-Rom
+        class Interpolating {
+          typedef std::vector<Nimble::Vector2> PointList;
+          typedef PointList::size_type Index;
+
+          PointList m_points;
+          PointList m_tangents;
+          Nimble::Vector2 get(Index ii, float h1, float h2, float h3, float h4) const;
+          Nimble::Vector2 getPoint(Index ii, float t) const;
+        public:
+          Nimble::Vector2 getDerivative(Index ii, float t) const;
+          Nimble::Vector2 get(float t) const;
+          void add(Nimble::Vector2 point);
+          void remove(Index ii);
+          Index size() const { return m_points.size(); }
+
+          friend class RenderContext;
+        };
 
     }
 
