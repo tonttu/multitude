6aa1239ad2c4b8192e14cc9f77d6fc0899b46b5c
Tommi Ilmonen <tommi@multitouch.fi>
2010-06-28 07:54:25 +0300

    Adding BGRA image format, and testing the performance of BGRA textures vs RGBA vs RGB. On OSX BGRA does not win.

--- multitude/Examples/GLBench/Main.cpp
+++ multitude/Examples/GLBench/Main.cpp
@@ -32,21 +32,24 @@ int main(int, char **)
   Luminous::GLResources::setThreadResources( & rsc, 0, 0);
 
   int i, j = 0;
-  const int levels = 13;
+  const int levels = 12;
   const int texturesperlevel = 5;
+  const int formatsperlevel = 3;
 
-  Luminous::Image images[levels];
+  Luminous::Image images[levels][formatsperlevel];
 
   for(i = 0; i < levels; i++) {
     int dim = 1 << i;
-    images[i].allocate(dim, dim, Luminous::PixelFormat::rgbUByte());
+    images[i][0].allocate(dim, dim, Luminous::PixelFormat::rgbUByte());
+    images[i][1].allocate(dim, dim, Luminous::PixelFormat::rgbaUByte());
+    images[i][2].allocate(dim, dim, Luminous::PixelFormat::bgraUByte());
   }
 
   info("Built the relevant images for testing.");
 
-  Luminous::Texture2D textures[texturesperlevel];
+  Luminous::Texture2D textures[texturesperlevel][formatsperlevel];
 
-
+  /* Test how long it takes to upload textures into the GPU, with different pixel formats. */
 
   for(int frame = 0; frame < 3; frame++) {
     SDL_Event event;
@@ -68,53 +71,73 @@ int main(int, char **)
 
     glClearColor(1.f, 0.f, 0.f, 1.f);
     glClear(GL_COLOR_BUFFER_BIT);
+    glEnable(GL_TEXTURE_2D);
 
-    info("\nFRAME %d", i);
+    info("\nFRAME %d", frame);
 
     for(i = 5; i < levels; i++) {
 
-      Radiant::TimeStamp t1 = Radiant::TimeStamp::getTime();
+      printf("\n");
 
-      for(j = 0; j < texturesperlevel; j++) {
-        // Create textures without loading:
-        textures[j].loadBytes(GL_RGB, images[i].width(), images[i].height(), 0,
-                              Luminous::PixelFormat::rgbUByte(), false);
+      for(int k = 0; k < formatsperlevel; k++) {
+
+        GLenum glLayout = (k == 0) ? GL_RGB : GL_RGBA;
+
+        Radiant::TimeStamp t1 = Radiant::TimeStamp::getTime();
+
+        for(j = 0; j < texturesperlevel; j++) {
+          // Create textures without loading:
+          textures[j][k].loadBytes(glLayout, images[i][k].width(), images[i][k].height(), 0,
+                                   images[i][k].pixelFormat(), false);
+          // Luminous::Utils::glTexRect(Nimble::Vector2(0,100), Nimble::Vector2(10, 110));
+
+        }
+
+
+        double createTime = t1.sinceSecondsD() * 1000 / texturesperlevel;
+
+        Luminous::Utils::glCheck("Texture test 1/3");
+
+        t1 = Radiant::TimeStamp::getTime();
+
+
+        for(j = 0; j < texturesperlevel; j++) {
+          // Create textures and load the actual data:
+          textures[j][k].loadBytes(glLayout, images[i][k].width(), images[i][k].height(),
+                                   images[i][k].data(),
+                                   images[i][k].pixelFormat(), false);
+          Luminous::Utils::glTexRect(Nimble::Vector2(0,0), Nimble::Vector2(10, 10));
+        }
+
+
+        double loadTime = t1.sinceSecondsD() * 1000 / texturesperlevel;
+
+        Luminous::Utils::glCheck("Texture test 2/3");
+
+        t1 = Radiant::TimeStamp::getTime();
+
+        for(j = 0; j < texturesperlevel; j++) {
+          // reload the actual data:
+          textures[j][k].bind();
+          glTexSubImage2D(GL_TEXTURE_2D, 0, 0, 0, images[i][k].width(), images[i][k].height(),
+                          images[i][k].pixelFormat().layout(), GL_UNSIGNED_BYTE, images[i][k].data());
+          Luminous::Utils::glTexRect(Nimble::Vector2(10,10), Nimble::Vector2(20, 20));
+        }
+
+        double subloadTime = t1.sinceSecondsD() * 1000 / texturesperlevel;
+
+        Luminous::Utils::glCheck("Texture test 3/3");
+
+        info("Texture dimensions %d %d, fmtindex = %d, create = %.3lf, load = %.3lf reload = %.3lf milliseconds",
+             images[i][k].width(), images[i][k].height(), k,
+             createTime, loadTime, subloadTime);
+
       }
-
-      double createTime = t1.sinceSecondsD() * 1000 / texturesperlevel;
-
-
-      t1 = Radiant::TimeStamp::getTime();
-
-      for(j = 0; j < texturesperlevel; j++) {
-        // Create textures and load the actual data:
-        textures[j].loadBytes(GL_RGB, images[i].width(), images[i].height(),
-                              images[i].data(),
-                              Luminous::PixelFormat::rgbUByte(), false);
-      }
-
-      double loadTime = t1.sinceSecondsD() * 1000 / texturesperlevel;
-
-      t1 = Radiant::TimeStamp::getTime();
-
-      for(j = 0; j < texturesperlevel; j++) {
-        // Create textures and load the actual data:
-        glTexSubImage2D(GL_TEXTURE_2D, 0, 0, 0, images[i].width(), images[i].height(),
-                        GL_RGB, GL_UNSIGNED_BYTE, images[i].data());
-      }
-
-      double subloadTime = t1.sinceSecondsD() * 1000 / texturesperlevel;
-
-      info("Texture dimensions %d %d, create = %.3lf, load = %.3lf reload = %.3lf milliseconds",
-           images[i].width(), images[i].height(),
-           createTime, loadTime, subloadTime);
-
 
     }
 
     SDL_GL_SwapBuffers();
   }
-
 
   return 0;
 }
