862298571425d0a9b969aacf14d39cf2cda6eae9
Esa Nuuros <esa@multitouch.fi>
2010-07-05 11:37:10 +0300

    Informative error about TR1 on Windows
    Added more informative error message about missing TR1 support on Windows

--- multitude/Radiant/Platform.hpp
+++ multitude/Radiant/Platform.hpp
@@ -1,6 +1,7 @@
 #ifndef RADIANT_PLATFORM_HPP
 #define RADIANT_PLATFORM_HPP
 
+// C++0x check
 #if __cplusplus > 199711L
   #define RADIANT_CPP0X 1
 #endif
@@ -11,6 +12,10 @@
 // Is this Windows?
 #elif WIN32
 #   define RADIANT_WIN32 1
+// Check for TR1
+#	ifndef _HAS_TR1
+#		error "Compiler TR1 support was not found. Please install Visual Studio 2008 Service Pack 1 or use a newer compiler."
+#	endif
 // Is this Linux?
 #elif __linux__
 #   define RADIANT_LINUX 1
