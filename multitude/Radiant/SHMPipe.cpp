2a36e45c25a01b271c9b43a92b2e6044993b3cdb
Riku Palomäki <riku@multitouch.fi>
2010-07-07 16:34:26 +0300

    Rewrote SHMPipe

--- multitude/Radiant/SHMPipe.cpp
+++ multitude/Radiant/SHMPipe.cpp
@@ -27,6 +27,7 @@
 #include <iostream>
 #include <memory>
 #include <sstream>
+#include <stdexcept>
 
 #include <string.h>
 
@@ -38,22 +39,104 @@
 
 namespace Radiant
 {
+  const char * shmError()
+  {
+    const char * str = strerror(errno);
+    errno = 0;
+    return str;
+  }
+
+  class SHMPipe::SHMHolder
+  {
+  public:
+    SHMHolder(key_t key, uint32_t size);
+    SHMHolder(int id);
+    ~SHMHolder();
+
+    void attach();
+
+    void * data() { return m_data; }
+    int size() const { return m_size; }
+
+  private:
+    void * m_data;
+    int m_id;
+    int m_size;
+  };
+
+  SHMPipe::SHMHolder::SHMHolder(key_t key, uint32_t size)
+    : m_size(size)
+  {
+    const char * const fnName = "SHMHolder::SHMHolder";
+
+/*    unsigned s = 1;
+    while(s < size)
+      s = s << 1;
+    m_size = size = s;*/
+
+    // Create the new SMA
+    /* shmget() rounds up size to nearest page size, so actual size
+       of area may be greater than requested size - however, this
+       does not affect anything, the extra will simply remain
+       unused */
+    m_id = shmget(key, sizeof(Data) + size, 0660 | IPC_EXCL | IPC_CREAT);
+    if(m_id != -1) {
+      debug("%s # Successfully created new shared memory area.", fnName);
+    } else {
+      error("%s # Failed to create new shared memory area (%s).",
+            fnName, shmError());
+      throw std::runtime_error("shmget failed");
+    }
+
+    attach();
+
+    // Mark the segment to be destroyed. It will be destroyed when
+    // last user detached it
+    if(shmctl(m_id, IPC_RMID, 0) != -1) {
+      debug("%s # Successfully destroyed shared memory area.", fnName);
+    } else {
+      error("%s # Failed to destroy shared memory area (%s).",
+            fnName, shmError());
+    }
+  }
+
+  SHMPipe::SHMHolder::SHMHolder(int id)
+    : m_id(id),
+    m_size(0)
+  {
+    attach();
+  }
+
+  SHMPipe::SHMHolder::~SHMHolder()
+  {
+    const char * const fnName = "SHMHolder::~SHMHolder";
+
+    if(shmdt(m_data) != -1) {
+      debug("%s # Successfully detached shared memory area.", fnName);
+    } else {
+      error("%s # Failed to detach shared memory area (%s).",
+            fnName, shmError());
+    }
+  }
+
+  void SHMPipe::SHMHolder::attach()
+  {
+    const char * const fnName = "SHMHolder::attach";
+
+    // Get pointer to SMA
+    m_data = shmat(m_id, 0, 0);
+    if(m_data != (void *)(-1)) {
+      debug("%s # Successfully obtained pointer %p to shared memory area.",
+            fnName, m_data);
+    } else {
+      error("%s # Failed to obtain pointer to shared memory area (%s)",
+            fnName, shmError());
+      throw std::runtime_error("shmat failed");
+    }
+  }
+
 
   // Class SHMPipe.
-
-
-  // Static data initialization.
-
-#ifdef WIN32
-  uint32_t  SHMPipe::smDefaultPermissions() { return PAGE_EXECUTE_READWRITE; }
-#else
-  // rw-rw-rw-.
-  uint32_t  SHMPipe::smDefaultPermissions() { return 0666; }
-#endif
-
-  // Set maximum buffer size to the largest possible value of a 32-bit integer less (header size + 1).
-  // uint32_t  SHMPipe::maxSize = 4294967295u - (smHeaderSize + 1);
-
 
   // Construction / destruction.
 
@@ -168,122 +251,19 @@ namespace Radiant
   }
   */
 #else
-  SHMPipe::SHMPipe(key_t smKey, uint32_t size)
-    : m_isCreator(false),
-      m_smKey(smKey),
-      m_id(-1),
-      m_size(0),
-      m_written(0),
-      m_read(0),
-      m_mask(0),
-      m_shm(0),
-      m_pipe(0)
+  SHMPipe::SHMPipe(key_t key, uint32_t size)
+    : m_holder(new SHMHolder(key, size)),
+    m_data(*reinterpret_cast<Data*>(m_holder->data()))
   {
-    const char * const  fnName = "SHMPipe::SHMPipe";
+    m_data.size = m_holder->size();
+    clear();
+  }
 
-    if(size > 0)
-    // Create new shared memory area (SMA)
-    {
-      unsigned s = 1;
-
-      while(s < size)
-	s = s << 1;
-
-      size = s;
-
-      // Clear any existing SMA with this key
-
-      const int id = shmget(m_smKey, 0, smDefaultPermissions());
-      if(id > 0) {
-        if(shmctl(id, IPC_RMID, 0) != -1) {
-	  debug("%s # Successfully removed existing shared memory area with same key.", fnName);
-        }
-        else {
-          error("%s # Failed to remove existing shared memory area with same key (%s).", fnName, strerror(errno));
-          assert(0);
-        }
-      }
-
-      // Create the new SMA
-
-      /* shmget() rounds up size to nearest page size, so actual size
-	 of area may be greater than requested size - however, this
-	 does not affect anything, the extra will simply remain
-	 unused */ 
-      m_id = shmget(m_smKey, SHM_HEADER_SIZE + size + 1,
-		    smDefaultPermissions() | IPC_EXCL | IPC_CREAT);
-      if(m_id != -1) {
-        m_isCreator = true;
-	debug("%s # Successfully created new shared memory area.", fnName);
-      }
-      else {
-        error("%s # Failed to create new shared memory area (%s).",
-	      fnName, shmError());
-        assert(0);
-      }
-    }
-    else
-    // Try to reference existing SMA
-    {
-      m_id = shmget(m_smKey, 0, smDefaultPermissions());
-      if(m_id != -1) {
-	debug("%s # Successfully accessed existing shared memory area",
-	      fnName);
-      }
-      else {
-        error("%s # Failed to access existing shared memory area (%s).", fnName, shmError());
-        assert(0);
-      }
-    }
-
-    // Get pointer to SMA
-
-    char * const  smPtr = (char *)(shmat(m_id, 0, 0));
-    if(smPtr != (char *)(-1)) {
-     debug("%s # Successfully obtained pointer %p to shared memory area.",
-	   fnName, smPtr);
-    }
-    else {
-      error("%s # Failed to obtain pointer to shared memory area (%s)",
-	    fnName, shmError());
-      assert(0);
-    }
-
-    m_shm = (uint8_t *) smPtr;
-
-    if(m_isCreator) {
-      m_size = size;
-      m_pipe = m_shm + SHM_PIPE_LOC;
-
-      // This is the creating object
-
-      // initialize header
-      bzero(m_shm, SHM_HEADER_SIZE);
-      bzero(m_pipe, m_size);
-      // write size to header
-      storeHeaderValue(SHM_SIZE_LOC, m_size);
-
-      info("Opened server SHMPipe with %u buffer bytes", (unsigned) m_size);
-    }
-    else {
-      m_size = readHeaderValue(SHM_SIZE_LOC);
-      m_pipe = m_shm + SHM_PIPE_LOC;
-      m_read = readHeaderValue(SHM_READ_LOC);
-
-      for(int i = 0; i < 64; i++) {
-	printf("%.2x ", m_pipe[i]);
-	if((i % 4) == 3)
-	  printf("\n");
-      }
-
-      fflush(0);
-
-      info("Opened client SHMPipe with %u buffer bytes", (unsigned) m_size);
-    }
-    
-    m_mask = m_size - 1;
-
-    // assert(isValid());
+  SHMPipe::SHMPipe(int id)
+    : m_holder(new SHMHolder(id)),
+    m_data(*reinterpret_cast<Data*>(m_holder->data()))
+  {
+    info("Opened client SHMPipe with %u buffer bytes", size());
   }
 #endif
 
@@ -322,71 +302,27 @@ namespace Radiant
   }
   */
 
-#else
-  SHMPipe::~SHMPipe()
-  {
-    const char * const  fnName = "SHMPipe::~SHMPipe";
-
-    // assert(isValid());
-
-    // Detach the SMA
-
-    char * const  smPtr = (char *) (m_shm);
-    if(shmdt(smPtr) != -1) {
-     debug("%s # Successfully detached shared memory area.", fnName);
-    }
-    else {
-      error("%s # Failed to detach shared memory area (%s).",
-	    fnName, shmError());
-    }
-
-    // Only the creating object can destroy the SMA, after the last detach, i.e. when no more
-    // objects are referencing it.
-
-    if(m_isCreator) {
-      if(shmctl(m_id, IPC_RMID, 0) != -1) {
-       debug("%s # Successfully destroyed shared memory area.", fnName);
-      }
-      else {
-        error("%s # Failed to destroy shared memory area (%s).",
-	      fnName, shmError());
-      }
-    }
-  }
 #endif
 
-
-  int SHMPipe::read(void * ptr, int n)
+  int SHMPipe::read(void * dest, int n)
   {
-    // int orig = n;
-
     uint32_t avail = readAvailable();
 
-    if( (int) avail < n) {
+    if(static_cast<int>(avail) < n) {
       //debug("SHMPipe::read # Only %d available, %d needed (%u %u)",
       // (int) avail, n, (unsigned) readPos(), (unsigned) writePos());
       return 0;
     }
 
-    uint8_t * dest = (uint8_t *) ptr;
-    const uint8_t  * pipe = m_pipe;
-
-    n = Nimble::Math::Min((uint32_t) n, avail);
-    uint32_t m = m_mask;
-    for(int i = 0; i < n; i++) {
-      dest[i] = pipe[(m_read + i) & m];
-      // printf("b[%d]: %x ", i, pipe[(m_read + i) & m]);
+    if(readPos() + n > size()) {
+      int n1 = size() - readPos();
+      memcpy(dest, m_data.pipe + readPos(), n1);
+      memcpy(reinterpret_cast<char*>(dest) + n1, m_data.pipe, n - n1);
+      m_data.readPos = n - n1;
+    } else {
+      memcpy(dest, m_data.pipe + readPos(), n);
+      m_data.readPos += n;
     }
-    
-    m_read += n;
-
-    storeHeaderValue(SHM_READ_LOC, m_read);
-
-    /*
-    if(n)
-      info("SHMPipe::read # Read %d vs %d (%d vs %d)",
-	   n, orig, readPos(), writePos());
-    */
 
     return n;
   }
@@ -399,17 +335,14 @@ namespace Radiant
 
     uint32_t n = read( & bytes, 4);
 
-    debug("SHMPipe::read # reading 4 header bytes, got %u (%u %u)",
-	  n, bytes, (m_read & m_mask));
-
     if(n != 4) {
       // debug("SHMPipe::read # could not read 4 bytes");
       return n;
     }
 
-    if(bytes > m_size) {
+    if(bytes > m_data.size) {
       error("SHMPipe::read # Too large object to read, stream corrupted %u",
-	    (int) bytes);
+            (int) bytes);
       return 0;
     }
 
@@ -418,8 +351,8 @@ namespace Radiant
     data.setTotal(n);
 
     if(n != bytes) {
-      error("SHMPipe::read # could not read final %d vs %d (%u %u)",
-	    n, (int) bytes, m_read, readPos());
+      error("SHMPipe::read # could not read final %d vs %d (%u)",
+            n, (int) bytes, readPos());
     }
 
     return n + 4;
@@ -429,130 +362,84 @@ namespace Radiant
   {
     uint32_t rp = readPos();
     uint32_t wp = writePos();
-    
-    return wp - rp;
+
+    return wp > rp ? wp - rp : wp + size() - rp;
   }
 
-  int SHMPipe::write(const void * ptr, int n)
+  int SHMPipe::write(const void * src, int n)
   {
-    // int orig = n;
-
     uint32_t avail = writeAvailable();
 
-    const uint8_t * src = (const uint8_t *) ptr;
-    uint8_t  * pipe = m_pipe;
+    n = Nimble::Math::Min<uint32_t>(n, avail);
 
-    n = Nimble::Math::Min((uint32_t) n, avail);
-    
-    int m = m_mask;
-    for(int i = 0; i < n; i++) {
-      pipe[(m_written + i) & m] = src[i];
+    if(writePos() + n > size()) {
+      int n1 = size() - writePos();
+      memcpy(m_data.pipe + m_data.written, src, n1);
+      memcpy(m_data.pipe, reinterpret_cast<const char*>(src) + n1, n - n1);
+      m_data.written = n - n1;
+    } else {
+      memcpy(m_data.pipe + m_data.written, src, n);
+      m_data.written += n;
     }
-
-    m_written += n;
-
-    /*if(n)
-      info("SHMPipe::write # Wrote %d vs %d (%d vs %d)",
-	   n, orig, readPos(), writePos());
-    */
     return n;
   }
 
   int SHMPipe::write(const BinaryData & data)
   {
-    uint32_t wavail = writeAvailable(data.pos() + 8);
-    if(wavail < (uint32_t) data.pos() + 8) {
+    uint32_t wavail = writeAvailable(data.pos() + 4);
+    if(wavail < (uint32_t) data.pos() + 4) {
       error("SHMPipe::write # Not enough space in the pipe (%u, %u < %u)",
-	    (unsigned) m_written, (unsigned) wavail, (unsigned) data.pos() + 8 );
+            (unsigned) m_data.written, (unsigned) wavail, (unsigned) data.pos() + 4 );
       return 0;
     }
 
     uint32_t bytes = data.pos();
-    if(write( & bytes, 4) != 4)
+    if(write(&bytes, 4) != 4)
       return 0;
 
     return write(data.data(), bytes) + 4;
   }
 
-  uint32_t SHMPipe::writeAvailable(int require)
+  uint32_t SHMPipe::writeAvailable()
   {
-    uint32_t rp = readPos() + size();
-    // uint32_t wp = writePos();
-    uint32_t wp = m_written;
-    
-    uint32_t avail = rp - wp;
-    if(avail)
-      avail--;
+    uint32_t rp = readPos();
+    uint32_t wp = m_data.written;
 
-    if(require) {
-      int times = 0;
+    return wp >= rp ? rp + size() - wp : rp - wp;
+  }
 
-      TimeStamp entry = TimeStamp::getTime();
+  uint32_t SHMPipe::writeAvailable(uint32_t require)
+  {
+    int times = 0;
+    uint32_t avail = writeAvailable();
 
-      while((int) avail < require && times < 100) {
-
-	/* if(!times) {
-	  info("SHMPipe::writeAvailable # Blocking");
-	}
-	*/
-	rp = readPos() + size();
-	wp = m_written;
-    
-	avail = rp - wp;
-	if(avail)
-	  avail--;
-
-	Sleep::sleepMs(2);
-	times++;
-      }
-
-      if(times) {
-	TimeStamp now = TimeStamp::getTime();
-
-	float spent = TimeStamp(now - entry).secondsD() * 1000.0;
-	if(spent > 100) {
-	  info("SHMPipe::writeAvailable # Blocked for %.3f avail %u vs %u",
-	       spent, avail, require);
-	}
-      }
+    while(avail < require && times++ < 100) {
+      Sleep::sleepMs(2);
+      avail = writeAvailable();
     }
-    
     return avail;
   }
 
   void SHMPipe::flush()
-  { 
-    storeHeaderValue(SHM_WRITE_LOC, m_written);
+  {
+    m_data.writePos = m_data.written;
     // info("SHMPipe::flush # Flushed out written data (%u)",(unsigned) m_written);
   }
 
-  void SHMPipe::zero()
+  void SHMPipe::clear()
   {
-    storeHeaderValue(SHM_WRITE_LOC, 0);
-    storeHeaderValue(SHM_READ_LOC, 0);
-    bzero(m_pipe, m_size);
-    m_written = 0;
-    m_read = 0;
-  }
-
-  const char * SHMPipe::shmError()
-  {
-    const char * str = strerror(errno);
-    errno = 0;
-    return str;
+    m_data.written = 0;
+    m_data.writePos = 0;
+    m_data.readPos = 0;
   }
 
   // Diagnostics.
-
   void SHMPipe::dump() const
   {
-    debug("m_isCreator = %s", m_isCreator ? "true" : "false");
 #ifdef WIN32
     debug("m_smName = %s", m_smName.c_str());
     debug("m_hMapFile = %p", m_hMapFile);
 #else
-    debug("m_smKey = %ul", (unsigned long)(m_smKey));
-    debug("m_id = %d", m_id);
 #endif
     debug("size() = %ul", (unsigned long)(size()));
 
