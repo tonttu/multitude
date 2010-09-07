e551d2db4d352a3c3394dad4a1e7b6c822705ebb
Aki Reijonen <aki@multitouch.fi>
2010-09-07 14:08:31 +0300

    use MULTI_CAM_OPEN_DELAY environment variable

--- multitude/Radiant/VideoCamera1394.cpp
+++ multitude/Radiant/VideoCamera1394.cpp
@@ -195,6 +195,7 @@ namespace Radiant {
 
   static dc1394_t * g_dc = 0;
   static int g_count = 0;
+  static int s_openDelay = 850;
 
   VideoCamera1394::VideoCamera1394(CameraDriver * driver)
       : VideoCamera(driver),
@@ -213,6 +214,13 @@ namespace Radiant {
 
     if(!g_dc)
       g_dc = dc1394_new();
+
+    if (g_count == 0) {
+      char * s = getenv("MULTI_CAM_OPEN_DELAY");
+      if (s) {
+        s_openDelay = atoi(s);
+      }
+    }
 
     g_count++;
   }
@@ -511,7 +519,7 @@ namespace Radiant {
     GuardStatic guard(&g_mutex);
 
     /* On some systems, sleep is needed for proper multi-camera operation. Sigh.*/
-    Radiant::Sleep::sleepMs(850);
+    Radiant::Sleep::sleepMs(s_openDelay);
 
     std::string videodevice("/dev/video1394");
 
@@ -650,7 +658,7 @@ namespace Radiant {
   {
     GuardStatic guard(&g_mutex);
 
-    Radiant::Sleep::sleepMs(850);
+    Radiant::Sleep::sleepMs(s_openDelay);
 
     const char * fname = "VideoCamera1394::openFormat7";
 
