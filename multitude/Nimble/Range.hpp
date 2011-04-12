1fbbbed3009ff9c747f9b863c72324c17b5f70a9
Tommi Ilmonen <tommi@multitouch.fi>
2011-04-12 15:13:04 +0300

    Added documentation to Nimble::Range.

--- multitude/Nimble/Range.hpp
+++ multitude/Nimble/Range.hpp
@@ -5,8 +5,11 @@
 
 namespace Nimble {
 
-  /** Numeric range representation. */
+  /** Numeric range representation.
 
+      By default the low, and high values are set to zero.
+
+  */
   template <class T>
   class NIMBLE_API RangeT
   {
@@ -14,13 +17,20 @@ namespace Nimble {
     RangeT(const T & low = 0, const T & high = 0)
       : m_low(low), m_high(high) {}
 
+    /// Check ifthe range is empty
+    /** @return This function returns true if the low value is equal to,
+        or exceeds the high value. */
     inline bool isEmpty() const { return m_low >= m_high; }
 
+    /// The difference between the upper and lower limit
     inline T span() const { return m_high - m_low; }
 
+    /// The lower limit of the range
     inline T low()  const { return m_low; }
+    /// The upper limit of the range
     inline T high() const { return m_high; }
 
+    /** Clamps the argument value to be between the low and high limits. */
     inline T clamp(const T & v) const
     {
       if(v <= m_low) return m_low;
@@ -28,6 +38,8 @@ namespace Nimble {
       return v;
     }
 
+    /// Compares two RangeT objects.
+    /** @return This function returns true if the two ranges are absolutely identical. */
     inline bool operator == (const RangeT & that) const
     {
       return m_low == that.m_low && m_high == that.m_high;
