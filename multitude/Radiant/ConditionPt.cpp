e9074c3edaa8e0583e47fca28d794a51502fda48
Tommi Ilmonen <tommi@multitouch.fi>
2011-04-14 22:32:50 +0300

    Mutex updates. Hopefully these mutexes now work.
    On POSIX, the mutex initialization is delayed, until the first call. QAtomicPointer is 
    used to ensure that the mutex is then initialized correctly.

--- multitude/Radiant/ConditionPt.cpp
+++ multitude/Radiant/ConditionPt.cpp
@@ -7,10 +7,10 @@
  * See file "Radiant.hpp" for authors and more details.
  *
  * This file is licensed under GNU Lesser General Public
- * License (LGPL), version 2.1. The LGPL conditions can be found in 
- * file "LGPL.txt" that is distributed with this source package or obtained 
+ * License (LGPL), version 2.1. The LGPL conditions can be found in
+ * file "LGPL.txt" that is distributed with this source package or obtained
  * from the GNU organization (www.gnu.org).
- * 
+ *
  */
 
 #include <Radiant/Condition.hpp>
@@ -26,76 +26,4 @@
 
 namespace Radiant
 {
-  class Condition::D
-  {
-    public:
-    pthread_cond_t m_ptcond;
-  };
-
-  Condition::Condition() 
-  : m_d(new D())
-  {
-    pthread_cond_init(&m_d->m_ptcond, 0);
-  }
-    
-  Condition::~Condition()
-  {
-    pthread_cond_destroy(&m_d->m_ptcond);
-    delete m_d;
-  }
-    
-  int Condition::wait(Mutex &mutex)
-  {
-    pthread_mutex_t * ptmutex = (pthread_mutex_t *)mutex.m_d;
-
-    return pthread_cond_wait(& m_d->m_ptcond, ptmutex);
-  }
-  
-  int Condition::wait(Mutex &mutex, int millsecs)
-  { 
-    struct timespec abstime;
-    struct timeval tv;
-
-    gettimeofday(&tv, 0);
-    tv.tv_sec += millsecs / 1000;
-    tv.tv_usec += 1000 * (millsecs % 1000);
-    if(tv.tv_usec >= 1000000)
-    {
-	    tv.tv_sec++;
-	    tv.tv_usec -= 1000000;
-    }
-    abstime.tv_sec = tv.tv_sec;
-    abstime.tv_nsec = 1000 * tv.tv_usec;
-
-    pthread_mutex_t * ptmutex = (pthread_mutex_t *)mutex.m_d;
-
-    return pthread_cond_timedwait(& m_d->m_ptcond, ptmutex, & abstime);
-  }
-    
-  int Condition::wakeAll() 
-  {
-    return pthread_cond_broadcast(& m_d->m_ptcond);
-  }
-    
-  int Condition::wakeAll(Mutex &mutex) 
-  { 
-    mutex.lock();
-    int r = pthread_cond_broadcast(& m_d->m_ptcond);
-    mutex.unlock();
-    return r;
-  }
-    
-  int Condition::wakeOne() 
-  {
-    return pthread_cond_signal(& m_d->m_ptcond);
-  }
-    
-  int Condition::wakeOne(Mutex &mutex) 
-  {
-    mutex.lock();
-    int r = wakeOne();
-    mutex.unlock();
-    return r;
-  }
-
 } // namespace
--- multitude/Radiant/Mutex.hpp
+++ multitude/Radiant/Mutex.hpp
@@ -1,16 +1,4 @@
 /* COPYRIGHT
- *
- * This file is part of Radiant.
- *
- * Copyright: MultiTouch Oy, Helsinki University of Technology and others.
- *
- * See file "Radiant.hpp" for authors and more details.
- *
- * This file is licensed under GNU Lesser General Public
- * License (LGPL), version 2.1. The LGPL conditions can be found in 
- * file "LGPL.txt" that is distributed with this source package or obtained 
- * from the GNU organization (www.gnu.org).
- * 
  */
 
 #ifndef RADIANT_MUTEX_HPP
@@ -30,44 +18,20 @@ namespace Radiant {
     ~Mutex();
 
     /// Lock the mutex, optionally blocking.
-    bool lock(bool block = true)
-    {
-      if(!m_initialized) {
-        initialize(m_recursive);
-        m_initialized = true;
-      }
-
-      return internalLock(block);
-    }
+    bool lock(bool block = true);
 
     /** Tries to lock the mutex. Does not block. */
-    bool tryLock()
-    {
-       if(!m_initialized) {
-         initialize(m_recursive);
-         m_initialized = true;
-       }
-
-       return internalTryLock();
-    }
+    bool tryLock() { return lock(false); }
 
     /// Unlocks the mutex.
     bool unlock();
 
-  private:    
-    bool initialize(bool recursive);
+  private:
 
-    bool internalLock(bool block);
-    bool internalTryLock();
-    bool internalUnlock();
-
-    bool m_initialized;
-    bool m_recursive;
+    friend class Condition;
 
     class D;
     D * m_d;
-
-    friend class Condition;
   };
 
   /** A guard class. This class is used to automatically lock and
