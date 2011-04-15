c3e7adceb41568d9271521825f5a03c3d00ea6eb
Tommi Ilmonen <tommi@multitouch.fi>
2011-04-15 15:05:19 +0300

    Fixed the Qt-based Mutex/Condition compile problems.

--- multitude/Radiant/ConditionQt.cpp
+++ multitude/Radiant/ConditionQt.cpp
@@ -13,63 +13,9 @@
  * 
  */
 
-#include <Radiant/Condition.hpp>
-
-#include <QWaitCondition>
-#include <QMutex>
 
 namespace Radiant
 {
-	class Condition::D : public QWaitCondition {};
 
-  Condition::Condition()
-	  : m_d(new D())
-  {
-  }
-    
-  Condition::~Condition()
-  {
-    delete m_d;
-  }
-    
-  int Condition::wait(Mutex &mutex)
-  {
-	  QMutex * qmutex = (QMutex *)(mutex.m_d);
-	  return m_d->wait(qmutex);
-  }
-  
-  int Condition::wait(Mutex &mutex, int millsecs)
-  { 
-	  QMutex * qmutex = (QMutex *)(mutex.m_d);
-	  return m_d->wait(qmutex, millsecs);
-  }
-    
-  int Condition::wakeAll() 
-  {
-	  m_d->wakeAll();
-    return 0;
-  }
-    
-  int Condition::wakeAll(Mutex &mutex) 
-  { 
-    mutex.lock();
-    wakeAll();
-    mutex.unlock();
-    return 0;
-  }
-    
-  int Condition::wakeOne() 
-  {
-	  m_d->wakeOne();
-    return 0;
-  }
-    
-  int Condition::wakeOne(Mutex &mutex) 
-  {
-    mutex.lock();
-    m_d->wakeOne();
-    mutex.unlock();
-    return 0;
-  }
 
 } // namespace
