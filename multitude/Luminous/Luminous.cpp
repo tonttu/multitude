712d1cbcc0b4e085b7a8b2f7d839fb4d9b715769
Esa Nuuros <esa@multitouch.fi>
2011-06-22 15:25:22 +0300

    Fixed some environment access in Windows

--- multitude/Luminous/Luminous.cpp
+++ multitude/Luminous/Luminous.cpp
@@ -111,8 +111,8 @@ namespace Luminous
     /// @todo does this work when the SDK is not installed? Where does find the plugins?
     char* dir = getenv("CORNERSTONE_ROOT");
     if(dir) {
-      dir = strcat(dir, "\\bin\\plugins");
-      QCoreApplication::addLibraryPath(dir);
+      std::string pluginPath = std::string(dir) + std::string("\\bin\\plugins");
+      QCoreApplication::addLibraryPath(pluginPath.c_str());
     }
 #endif
 
