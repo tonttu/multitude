6941e2c6dab8656d9f3fa7a9612063608e31593c
Esa Nuuros <esa@multitouch.fi>
2011-08-18 16:29:18 +0300

    Documented the Singleton
    Fixes #1971.

--- multitude/Radiant/Singleton.hpp
+++ multitude/Radiant/Singleton.hpp
@@ -20,28 +20,56 @@
 #include "Mutex.hpp"
 #include "RefPtr.hpp"
 
-namespace Radiant {
-  /// Shared mutex for all the DEFINE_SINGLETON macros
-  extern RADIANT_API Radiant::Mutex s_singletonMutex;
-}
-
-/** Implements singleton of object type T
+/*!
+    @page singleton Radiant::Singleton<T>
+    Implements singleton of object type T
 
     Singleton is used when there is only one object of type T. The one object
-    can be accessed with the function instance(). The object is created as you
-    for the first time access it. The lazy creation is done because it might
-    not be possible to create objects during application startup.
+    can be accessed with the function instance(). The object is created the
+    first time you access it. The lazy creation is done because it might
+    not be possible to create objects during application startup statically.
 
     The singleton instance is destroyed when its reference count reaches zero.
     So if you want to guarantee the life-time of the singleton instance, you
     should store the returned shared_ptr.
 
-    @todo document the usage of these macros
-*/
+    To declare a class as singleton, you use the DECLARE_SINGLETON macro in your
+    class declaration:
 
-/// This macro implements "double-checked locking pattern" to minimize the
-/// mutex usage. In almost all cases the mutex doesn't need to be locked, so
-/// using only one static mutex won't slow things down.
+    @code
+    // Declare a MyClass and make it a singleton
+    class MyClass {
+      DECLARE_SINGLETON(MyClass);
+      ...
+    };
+    @endcode
+
+    You must also define the singleton using DEFINE_SINGLETON macro. Usually
+    you put this in the .cpp file with the rest of the class definition:
+
+    @code
+    #include "MyClass.hpp"
+
+    // Define the MyClass singleton
+    DEFINE_SINGLETON(MyClass);
+    @endcode
+
+    The DEFINE_SINGLETON macro implements "double-checked locking pattern" to
+    minimize the mutex usage. In almost all cases the mutex doesn't need to be
+    locked, so using only one shared static mutex for all singletons won't slow
+    things down.
+
+    Then to actually use the singleton, you just call the instance() method
+    which returns a shared pointer.
+
+    @code
+    // Do something with the singleton
+    std::shared_ptr<MyClass> ptr = MyClass::instance();
+
+    ptr->doSomething();
+    @endcode
+
+    This class is thread-safe. */
 #define DEFINE_SINGLETON(T)                                        \
   std::shared_ptr<T> T :: instance() {                             \
     std::shared_ptr<T> p = s_multiSingletonInstance.lock();        \
@@ -59,5 +87,11 @@ namespace Radiant {
 #define DECLARE_SINGLETON(T)                                       \
   public: static std::shared_ptr<T> instance();                    \
   private: static std::weak_ptr<T> s_multiSingletonInstance
+
+namespace Radiant {
+
+  /// Shared mutex for all the @ref singleton macros
+  extern RADIANT_API Radiant::Mutex s_singletonMutex;
+}
 
 #endif
