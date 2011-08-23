tonttu/multitude

commit f858e81a8e16372a8d5d0ae50bcfc8b70ebb1796

Fixed TCPSocket blocking

multitude/Radiant/TCPSocketPosix.cpp
@@ -129,7 +129,7 @@ namespace Radiant
     return m_d->m_fd >= 0;
   }
 
-  int TCPSocket::read(void * buffer, int bytes, bool waitfordata)
+  int TCPSocket::read(void * buffer, int bytes, Flags flags)
   {
     if(m_d->m_fd < 0 || bytes < 0)
       return -1;
@@ -141,21 +141,20 @@ namespace Radiant
       errno = 0;
       // int max = bytes - pos > SSIZE_MAX ? SSIZE_MAX : bytes - pos;
       int max = bytes - pos > 32767 ? 32767 : bytes - pos;
-      int tmp = recv(m_d->m_fd, data + pos, max, 0);
+      bool block = flags == WAIT_ALL || (flags == WAIT_SOME && pos == 0);
+      int tmp = recv(m_d->m_fd, data + pos, max, block ? MSG_DONTWAIT : 0);
 
       if(tmp > 0) {
         pos += tmp;
       } else if(tmp == 0 || m_d->m_fd == -1) {
         return pos;
       } else if(errno == EAGAIN || errno == EWOULDBLOCK) {
-        if(waitfordata) {
-          struct pollfd pfd;
-          pfd.fd = m_d->m_fd;
-          pfd.events = POLLIN;
-          wrap_poll(&pfd, 1, 5000);
-        } else {
-          return pos;
-        }
+        if(!block) return pos;
+
+        struct pollfd pfd;
+        pfd.fd = m_d->m_fd;
+        pfd.events = POLLIN;
+        wrap_poll(&pfd, 1, 5000);
       } else {
         error("TCPSocket::read # Failed to read: %s", wrap_strerror(wrap_errno));
         return pos;
@@ -163,38 +162,6 @@ namespace Radiant
     }
 
     return pos;
-  }
-
-  int TCPSocket::readSome(void * buffer, int bytes, bool waitfordata)
-  {
-    if(m_d->m_fd < 0 || bytes < 0)
-      return -1;
-
-    if(bytes > 32767)
-      bytes = 32767;
-
-    for(;;) {
-      errno = 0;
-      int tmp = recv(m_d->m_fd, (char*)buffer, bytes, 0);
-
-      if(tmp > 0) {
-        return tmp;
-      } else if(errno == EAGAIN || errno == EWOULDBLOCK) {
-        if(waitfordata) {
-          struct pollfd pfd;
-          pfd.fd = m_d->m_fd;
-          pfd.events = POLLIN;
-          wrap_poll(&pfd, 1, 5000);
-        } else {
-          return 0;
-        }
-      } else {
-        error("TCPSocket::readSome # Failed to read: %s", wrap_strerror(wrap_errno));
-        return 0;
-      }
-    }
-
-    return 0;
   }
 
   int TCPSocket::write(const void * buffer, int bytes)
@@ -261,7 +228,13 @@ namespace Radiant
     return (pfd.revents & POLLRDNORM) == POLLRDNORM;
   }
 
-  void TCPSocket::moveToThread(Thread *) {
+  void TCPSocket::moveToThread(Thread *)
+  {
+  }
+
+  int TCPSocket::fd() const
+  {
+    return m_d->m_fd;
   }
 
 }

multitude/Radiant/TCPSocket.hpp
@@ -33,6 +33,14 @@ namespace Radiant {
   class RADIANT_API TCPSocket : public Radiant::BinaryStream, public Patterns::NotCopyable
   {
   public:
+    /// Flags used with read() -function
+    enum Flags
+    {
+      NONBLOCK,   ///< Never block
+      WAIT_SOME,  ///< Block until some data is read or socket has a read error
+      WAIT_ALL    ///< Blocks until all requested data is read or socket has a read error
+    };
+
     TCPSocket();
     /// Construct a socket based on a file descriptor
     /// This method is potentially non-portable as not all platforms use file
@@ -67,25 +75,35 @@ namespace Radiant {
     /// Read bytes from the socket
     /** @param[out] buffer pointer to a buffer to store the read data to
         @param bytes how many bytes the buffer has room for
-        @param waitfordata Conditionally wait for all the data to arrive.
+        @param flags Blocking behaviour
         @return the number of bytes actually read
         */
-    int read(void * buffer, int bytes, bool waitfordata = true);
+    int read(void * buffer, int bytes, Flags flags);
+
+    /// Read bytes from the socket
+    /** @param[out] buffer pointer to a buffer to store the read data to
+        @param bytes how many bytes the buffer has room for
+        @param waitfordata wait for all data or do not block at all
+        @see read(void*,int,Flags)
+        @return the number of bytes actually read
+        */
+    virtual int read(void * buffer, int bytes, bool waitfordata = true)
+    {
+      return read(buffer, bytes, waitfordata ? WAIT_ALL : NONBLOCK);
+    }
+
     /// Write bytes to the socket
     int write(const void * buffer, int bytes);
-
-    /// Read some bytes from the socket
-    /// @param[out] buffer to write to
-    /// @param bytes bytes to read
-    /// @param waitfordata conditionally wait for some data to arrive
-    /// @return number of bytes read
-    int readSome(void * buffer, int bytes, bool waitfordata = true);
 
     /// Returns true if the socket has been closed
     bool isHungUp() const;
 
     /// Return 'true' if readable data is pending.
     bool isPendingInput(unsigned int waitMicroSeconds = 0);
+
+    /// @cond
+    int fd() const;
+    /// @endcond
 
 /// @cond
 

commit 64e3ff01dc17d2d81368968bf7dad5752b965248

Fixed tcp socket that was broken in commit:f858e81a8e16372a8d

multitude/Radiant/TCPSocketPosix.cpp
@@ -13,6 +13,7 @@
  * 
  */
 
+#include "Platform.hpp"
 #include "TCPSocket.hpp"
 #include "SocketUtilPosix.hpp"
 #include "SocketWrapper.hpp"
@@ -142,7 +143,19 @@ namespace Radiant
       // int max = bytes - pos > SSIZE_MAX ? SSIZE_MAX : bytes - pos;
       int max = bytes - pos > 32767 ? 32767 : bytes - pos;
       bool block = flags == WAIT_ALL || (flags == WAIT_SOME && pos == 0);
-      int tmp = recv(m_d->m_fd, data + pos, max, block ? MSG_DONTWAIT : 0);
+      int tmp;
+
+      if(block) {
+        tmp = recv(m_d->m_fd, data + pos, max, 0);
+      } else {
+#ifdef RADIANT_WINDOWS
+        // there is no MSG_DONTWAIT in winsock, do poll with zero timeout instead
+        if(!isPendingInput()) return pos;
+        tmp = recv(m_d->m_fd, data + pos, max, 0);
+#else
+        tmp = recv(m_d->m_fd, data + pos, max, MSG_DONTWAIT);
+#endif
+      }
 
       if(tmp > 0) {
         pos += tmp;