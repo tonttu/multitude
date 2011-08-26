c4cef7f41ed25602fcd9a65c926b27ab85ad8fb2
Riku Palomäki <riku@multitouch.fi>
2011-08-26 11:24:14 +0300

    Fixed #2001, BinaryData::writeString("") was broken

--- multitude/Radiant/BinaryData.cpp
+++ multitude/Radiant/BinaryData.cpp
@@ -136,10 +136,7 @@ namespace Radiant {
 
     getRef<int32_t>() = STRING_MARKER;
     char * ptr =  getPtr<char>(space);
-    if(len)
-      memcpy(ptr, s, len + 1);
-    else
-      getRef<int32_t>() = 0;
+    memcpy(ptr, s, len + 1);
   }
 
   void BinaryData::writeWString(const std::wstring & str)
