37c57be654ee04dd2a7b4a181ae6a747d48101b3
Tommi Ilmonen <tommi@multitouch.fi>
2011-08-29 08:35:26 +0300

    Fixed OSX compile problem in Texture.cpp.
    The GL_RGBA32F enums are not defined on OSX Snow Leopard it seems...

--- multitude/Luminous/Texture.cpp
+++ multitude/Luminous/Texture.cpp
@@ -1,16 +1,4 @@
 /* COPYRIGHT
- *
- * This file is part of Luminous.
- *
- * Copyright: MultiTouch Oy, Helsinki University of Technology and others.
- *
- * See file "Luminous.hpp" for authors and more details.
- *
- * This file is licensed under GNU Lesser General Public
- * License (LGPL), version 2.1. The LGPL conditions can be found in 
- * file "LGPL.txt" that is distributed with this source package or obtained 
- * from the GNU organization (www.gnu.org).
- * 
  */
 
 #include "Texture.hpp"
@@ -146,11 +134,16 @@ namespace Luminous
     case GL_BGR:
       used *= 3;
       break;
+#ifdef GL_RGB32F
     case GL_RGB32F:
       used *= 3 * 4;
       break;
+#endif
+#ifdef GL_RGBA32F
     case GL_RGBA32F:
       used *= 4 * 4;
+      break;
+#endif
     default:
       used *= 4;
       break;
@@ -448,7 +441,7 @@ namespace Luminous
     if(m_haveMipmaps)
     {
       /// @todo mipmap support should be implemented
-      error("Texture2D::loadSubBytes # Cannot be used with mipmaps");     
+      error("Texture2D::loadSubBytes # Cannot be used with mipmaps");
     } else {
 
       int alignment = 1;
