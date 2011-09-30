tonttu/multitude

commit 55ecb0b75bf3ae0e9811cae02f270b26535448e7

Set default visibility to hidden in gcc Fixed some symbol visibility issues Made Box2D a static library on all platforms (it was already on Windows)

multitude/multitude.pri
@@ -11,6 +11,9 @@ DEPENDPATH += $$PWD
 
 # The Cornerstone version for libraries
 unix {
+	# Make symbol export compatible with MSVC
+  QMAKE_CXXFLAGS += -fvisibility-ms-compat
+
   MULTITUDE_VERSION_MAJOR=$$system(cat ../VERSION | cut -d . -f 1)
   MULTITUDE_VERSION_MINOR=$$system(cat ../VERSION | cut -d . -f 2)
   MULTITUDE_VERSION_PATCH=$$system(cat ../VERSION | cut -d . -f 3 | cut -d - -f 1)

multitude/Box2D/Box2D.pro
@@ -2,7 +2,7 @@ include(../multitude.pri)
 
 CONFIG -= qt
 
-win32:CONFIG += staticlib
+CONFIG += staticlib
 
 S1 += Collision/b2BroadPhase.cpp \
   Collision/b2CollideCircle.cpp \

multitude/Luminous/Luminous.pro
@@ -84,7 +84,7 @@ LIBS += $$LIB_RADIANT \
     $$LIB_PATTERNS \
     $$LIB_GLEW
 
-win32:DEFINES += LUMINOUS_EXPORT
+DEFINES += LUMINOUS_EXPORT
 
 CONFIG += qt
 QT += gui

multitude/Nimble/Nimble.pro
@@ -39,6 +39,6 @@ SOURCES += Rect.cpp
 SOURCES += Splines.cpp
 SOURCES += Vector2.cpp
 
-win32:DEFINES += NIMBLE_EXPORT
+DEFINES += NIMBLE_EXPORT
 
 include(../library.pri)

multitude/Poetic/Poetic.pro
@@ -56,8 +56,9 @@ macx:LIBS += -framework,OpenGL
 
 include(../library.pri)
 
+DEFINES += POETIC_EXPORT
+
 win32 {
-	DEFINES += POETIC_EXPORT
 
 	win64 {
 		INCLUDEPATH += ..\\Win64x\\include\\freetype2

multitude/Radiant/Radiant.pro
@@ -127,6 +127,8 @@ linux-*: LIBS += -lX11 -lXtst
 
 macx:LIBS += -framework,CoreFoundation
 
+DEFINES += RADIANT_EXPORT
+
 unix {
   LIBS += -lpthread $$LIB_RT -ldl
   PKGCONFIG += libdc1394-2
@@ -137,7 +139,6 @@ unix {
 
 win32 {
     message(Radiant on Windows)
-    DEFINES += RADIANT_EXPORT
     # CMU driver is only 32-bit 
     !win64 {
        DEFINES += CAMERA_DRIVER_CMU

multitude/Radiant/VideoCamera1394.hpp
@@ -100,7 +100,7 @@ namespace Radiant {
     virtual int framesBehind() const { return m_framesBehind; }
 
     /// Reset all Firewire buses
-    static void busReset();
+    RADIANT_API static void busReset();
 
   private:
     bool enableCameraFeature(unsigned int feature,

multitude/Radiant/XFaker.hpp
@@ -17,6 +17,7 @@
 #define XFAKER_HPP
 
 #include "Platform.hpp"
+#include "Export.hpp"
 
 #ifdef RADIANT_LINUX
 
@@ -30,15 +31,15 @@ namespace Radiant {
   {
   public:
     /// Constructs XFaker for the given display
-    XFaker(const char* displayName = 0);
-    ~XFaker();
+    RADIANT_API XFaker(const char* displayName = 0);
+    RADIANT_API ~XFaker();
 
     /// Fakes mouse move event to given coordinates 
-    void fakeMouseMove(int x, int y);
+    RADIANT_API void fakeMouseMove(int x, int y);
     /// Fakes mouse button press
-    void fakeMouseButton(int button, bool press);
+    RADIANT_API void fakeMouseButton(int button, bool press);
     /// Fakes mouse wheel event
-    void fakeMouseWheel(int dx, int dy);
+    RADIANT_API void fakeMouseWheel(int dx, int dy);
 
   private:
 

commit ada932b4a78cb4379491ab31169c9ff862ac471b

Fix OS X build
Broken in commit:55ecb0b75bf3ae0e9811cae02f270b26535448e7

multitude/multitude.pri
@@ -101,8 +101,6 @@ macx {
     LIB_VALUABLE = -framework,Valuable
     LIB_VIDEODISPLAY = -framework,VideoDisplay
     LIB_PATTERNS = -framework,Patterns
-
-    LIB_BOX2D = -framework,Box2D
   }
 
   # change architecture to x86_64 if snow leopard