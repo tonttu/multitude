6d7043c35a7bc070e65359e57514c166629f42b9
Jeroen Akershoek <jeroen@multitou.ch>
2012-05-24 14:45:12 +0300

    Fixed argument passing bug in the Luminous2 hardware buffer

--- multitude/Luminous/HardwareBuffer.cpp
+++ multitude/Luminous/HardwareBuffer.cpp
@@ -48,7 +48,7 @@ namespace Luminous
     std::copy(start, start + bytes, data);
   }
 
-  void HardwareBuffer::write(const char * data, size_t bytes, size_t offset)
+  void HardwareBuffer::write(const char * data, size_t offset, size_t bytes)
   {
     assert(offset + bytes <= m_d->data.size());
     char * start = m_d->data.data() + offset;
