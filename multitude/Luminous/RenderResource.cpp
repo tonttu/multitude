98d8e532b5fd51fe8b6fb5f351474aa6115378ec
Tommi Ilmonen <tommi@multitouch.fi>
2012-06-07 17:46:16 +0300

    RenderDriverGL/RenderResource: Removed some compilation warnings.

--- multitude/Luminous/RenderResource.cpp
+++ multitude/Luminous/RenderResource.cpp
@@ -4,8 +4,8 @@
 namespace Luminous
 {
   RenderResource::RenderResource(Id id, ResourceType type, RenderDriver & driver)
-    : m_id(id)
-    , m_generation(0)
+    : m_generation(0)
+    , m_id(id)
     , m_type(type)
     , m_driver(driver)
   {
