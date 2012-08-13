06f9c7f8e6ddfcc3c4d2a3552fb87b276aaa6e3b
Aki Reijonen <aki@multitouch.fi>
2012-08-13 18:23:10 +0300

    Better declaration for deleted RenderManager constructor

--- multitude/Luminous/RenderManager.hpp
+++ multitude/Luminous/RenderManager.hpp
@@ -48,7 +48,7 @@ namespace Luminous
     template <typename T> static T * getResource( RenderResource::Id id );
 
   private:
-    LUMINOUS_API RenderManager();
+    RenderManager() = delete;
   };
 }
 
