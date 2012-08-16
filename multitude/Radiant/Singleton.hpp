a2e9b7e87b3d3b4c63159f49e181a5cdb5374c50
Esa Nuuros <esa@multitouch.fi>
2012-08-16 14:42:06 +0300

    Added note about Radiant::Singleton macro
    Singletons can not be exported.

--- multitude/Radiant/Singleton.hpp
+++ multitude/Radiant/Singleton.hpp
@@ -84,6 +84,8 @@
   }                                                                \
   std::weak_ptr<T> T::s_multiSingletonInstance;
 
+/// @todo this can not be used to properly export the instace() function from
+/// libraries. The macro needs one more parameter.
 #define DECLARE_SINGLETON(T)                                       \
   public: static std::shared_ptr<T> instance();                    \
   private: static std::weak_ptr<T> s_multiSingletonInstance
