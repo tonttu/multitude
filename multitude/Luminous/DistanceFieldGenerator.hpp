4aa80f2b9ef950851903764d0cc861445bacff3a
Tommi Ilmonen <tommi@multitouch.fi>
2012-09-28 14:59:46 +0300

    Made DistanceFieldGenerator a class rather than a namespace.
    This cleans up the Doxygen documentation a bit (fewer namespaces).

--- multitude/Luminous/DistanceFieldGenerator.hpp
+++ multitude/Luminous/DistanceFieldGenerator.hpp
@@ -7,14 +7,15 @@
 
 namespace Luminous
 {
-  namespace DistanceFieldGenerator
+  class DistanceFieldGenerator
   {
+  public:
     /// Generates (usually low-resolution) distance field from a (high-resolution) src image
     /// Both images should be grayscale 8 bit images
     /// @param radius search neighbourhood size in src texels
-    LUMINOUS_API void generate(const Luminous::Image & src, Nimble::Vector2i srcSize,
+    static  void LUMINOUS_API generate(const Luminous::Image & src, Nimble::Vector2i srcSize,
                                Luminous::Image & target, int radius);
-  }
+  };
 }
 
 #endif // LUMINOUS_DISTANCE_FIELD_GENERATOR_HPP
