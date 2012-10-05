b87f6ef76e575b62a5859bdf59e21d4f158ade87
Sebastian Eriksson <sebastian@multitouch.fi>
2012-10-05 17:03:25 +0300

    Fixed PostProcess filter and experimental

--- multitude/Luminous/PostProcessFilter.cpp
+++ multitude/Luminous/PostProcessFilter.cpp
@@ -4,20 +4,42 @@
 
 namespace Luminous
 {
-  PostProcessFilter::PostProcessFilter()
-    : m_enabled(true)
+  class PostProcessFilter::D
   {
-    m_renderTarget.attach(GL_COLOR_ATTACHMENT0, m_framebuffer);
-    m_renderTarget.attach(GL_DEPTH_ATTACHMENT, m_depthBuffer);
+  public:
+    D()
+      : m_enabled(true)
+      , m_order(0)
+    {}
+
+    bool m_enabled;
+    int m_order;
+
+    Luminous::RenderTarget m_renderTarget;
+
+    Luminous::Texture m_framebuffer;
+    Luminous::RenderBuffer m_depthBuffer;
+  };
+
+  ////////////////////////////////////////////////////////////
+  ////////////////////////////////////////////////////////////
+
+  PostProcessFilter::PostProcessFilter()
+    : m_d(new D())
+  {
   }
 
   PostProcessFilter::~PostProcessFilter()
   {
+    delete m_d;
   }
 
   void PostProcessFilter::initialize(RenderContext & rc)
   {
-    m_renderTarget.setSize(Nimble::Size(rc.contextSize().x, rc.contextSize().y));
+    m_d->m_renderTarget.attach(GL_COLOR_ATTACHMENT0, m_d->m_framebuffer);
+    m_d->m_renderTarget.attach(GL_DEPTH_ATTACHMENT, m_d->m_depthBuffer);
+
+    m_d->m_renderTarget.setSize(Nimble::Size(rc.contextSize().x, rc.contextSize().y));
   }
 
   Luminous::Style PostProcessFilter::style() const
@@ -25,7 +47,7 @@ namespace Luminous
     Luminous::Style style;
 
     style.setFillColor(1.0f, 1.0f, 1.0f, 1.0f);
-    style.setTexture("tex", m_framebuffer);
+    style.setTexture("tex", texture());
 
     return style;
   }
@@ -52,5 +74,45 @@ namespace Luminous
     b.vertex[2].texCoord.make(0, 1);
     b.vertex[3].location.make(size.x, size.y);
     b.vertex[3].texCoord.make(1, 1);
+  }
+
+  bool PostProcessFilter::enabled() const
+  {
+    return m_d->m_enabled;
+  }
+
+  void PostProcessFilter::setEnabled(bool enabled)
+  {
+    m_d->m_enabled = enabled;
+  }
+
+  int PostProcessFilter::order() const
+  {
+    return m_d->m_order;
+  }
+
+  void PostProcessFilter::setOrder(int order)
+  {
+    m_d->m_order = order;
+  }
+
+  Luminous::RenderTarget & PostProcessFilter::renderTarget()
+  {
+    return m_d->m_renderTarget;
+  }
+
+  const Luminous::RenderTarget & PostProcessFilter::renderTarget() const
+  {
+    return m_d->m_renderTarget;
+  }
+
+  const Luminous::Texture & PostProcessFilter::texture() const
+  {
+    return m_d->m_framebuffer;
+  }
+
+  const Luminous::RenderBuffer & PostProcessFilter::depthBuffer() const
+  {
+    return m_d->m_depthBuffer;
   }
 }
--- multitude/Luminous/PostProcessFilter.hpp
+++ multitude/Luminous/PostProcessFilter.hpp
@@ -9,15 +9,26 @@ namespace Luminous
 {
   /// This class represents one filter in the post process queue that is
   /// applied to the scene before rendering the final image.
+  ///
+  /// To create your own custom post processing filter by using a custom shader
+  /// you should derive this class and override the appropriate functions. For
+  /// most cases overriding style() is sufficient. By default the the filter uses
+  /// the style from style() to render a context-sized quad to the bound render
+  /// target.
   class LUMINOUS_API PostProcessFilter : public Patterns::NotCopyable
   {
   public:
+    /// Creates a new post process filter
     PostProcessFilter();
     virtual ~PostProcessFilter();
 
+    /// Initializes the filter. By default attaches a color and depth attachments
+    /// to the render target and resizes the render target and attachments.
     virtual void initialize(Luminous::RenderContext & rc);
 
     /// Returns the style that is used to render the scene
+    /// Override this function if you want to specify a custom style,
+    /// ie. for using a custom shader.
     virtual Luminous::Style style() const;
 
     /// Clears the render target
@@ -25,27 +36,30 @@ namespace Luminous
     /// Draws a context sized quad with style from calling style().
     virtual void apply(Luminous::RenderContext & rc);
 
-    /// @todo allow user to add own uniform block
-    //template<typename UniformBlock>
-    //virtual void applyUniforms(UniformBlock * b);
+    /// If the filter is disabled, it is skipped
+    /// @return true if enabled, false otherwise
+    bool enabled() const;
 
-    bool enabled() const { return m_enabled; }
-    void setEnabled(bool enabled) { m_enabled = enabled; }
+    /// Sets the enabled state of the filter
+    void setEnabled(bool enabled);
 
-    int order() const { return m_order; }
-    void setOrder(int order) { m_order = order; }
+    int order() const;
+    void setOrder(int order);
 
-    const Luminous::RenderTarget & renderTarget() const { return m_renderTarget; }
+    Luminous::RenderTarget & renderTarget();
+    const Luminous::RenderTarget & renderTarget() const;
 
-  protected:
-    Luminous::RenderTarget m_renderTarget;
+    /// Color buffer texture used for rendering the scene.
+    /// Use this texture as the source for the post processing filter.
+    /// @return color buffer texture
+    const Luminous::Texture & texture() const;
 
-    Luminous::Texture m_framebuffer;
-    Luminous::RenderBuffer m_depthBuffer;
+    /// Depth buffer used for rendering the scene.
+    const Luminous::RenderBuffer & depthBuffer() const;
 
   private:
-    bool m_enabled;
-    int m_order;
+    class D;
+    D * m_d;
   };
 }
 #endif // POSTPROCESSOR_HPP
