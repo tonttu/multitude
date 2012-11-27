f450437099745a012a793fe064fb12746936cf28
Jeroen Akershoek <jeroen@multitou.ch>
2012-11-27 09:50:25 +0200

    Added polygon rendering primitive

--- multitude/Luminous/Luminous.hpp
+++ multitude/Luminous/Luminous.hpp
@@ -146,6 +146,7 @@ namespace Luminous
   //////////////////////////////////////////////////////////////////////////
   enum PrimitiveType
   {
+    PrimitiveType_Polygon       = GL_POLYGON,
     PrimitiveType_Triangle      = GL_TRIANGLES,
     PrimitiveType_TriangleStrip = GL_TRIANGLE_STRIP,
     PrimitiveType_TriangleFan   = GL_TRIANGLE_FAN,
