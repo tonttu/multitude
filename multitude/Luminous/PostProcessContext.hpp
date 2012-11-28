888bfa6344546115c0981a807664a03aa7418987
Sebastian Eriksson <sebastian@multitouch.fi>
2012-11-28 16:24:29 +0200

    Added documentation to post processing classes

--- multitude/Luminous/PostProcessContext.hpp
+++ multitude/Luminous/PostProcessContext.hpp
@@ -8,35 +8,34 @@
 
 namespace Luminous
 {
-  /// This class represents one filter in the post process queue that is
-  /// applied to the scene before rendering the final image.
-  ///
-  /// To create your own custom post processing filter by using a custom shader
-  /// you should derive this class and override the appropriate functions. For
-  /// most cases overriding style() is sufficient. By default the the filter uses
-  /// the style from style() to render a context-sized quad to the bound render
-  /// target.
+  /// PostProcessContext is the render-thread specific context that is used
+  /// for post processing to work in a multi-context environment.
+  /// Use PostProcesFilter for defining custom filters.
   class LUMINOUS_API PostProcessContext : public Patterns::NotCopyable
   {
   public:
     /// Creates a new post process filter
+    /// @param filter filter that this context belongs to
     PostProcessContext(const PostProcessFilterPtr filter);
     virtual ~PostProcessContext();
 
     /// Initializes the filter. By default attaches a color and depth attachments
     /// to the render target and resizes the render target and attachments.
+    /// @param rc Context of the current render thread
     void initialize(Luminous::RenderContext & rc);
 
+    /// Performs the filtering operation by setting the style values and
+    /// calling PostProcessFilter::filter()
+    /// @param rc Context of the current render thread
     void doFilter(Luminous::RenderContext & rc);
 
-    /// If the filter is disabled, it is skipped
-    /// @return true if enabled, false otherwise
+    /// Checks if the filter is enabled, disabled filters will be skipped.
+    /// @return true if enabled, false otherwise.
     bool enabled() const;
 
-    /// Sets the enabled state of the filter
-    void setEnabled(bool enabled);
-
+    /// Returns the render target used for rendering the scene
     Luminous::RenderTarget & renderTarget();
+    /// @copydoc renderTarget
     const Luminous::RenderTarget & renderTarget() const;
 
     /// Color buffer texture used for rendering the scene.
--- multitude/Luminous/PostProcessFilter.hpp
+++ multitude/Luminous/PostProcessFilter.hpp
@@ -10,22 +10,54 @@ namespace Luminous
   class PostProcessContext;
   class RenderContext;
 
+  /** Post-processing filters are tools that can be used to process the final
+      output image of a rendered scene. Each filter describes one post-processing
+      pass that is applied on the whole window context. The post-processing
+      framework works by rendering the whole scene into an off-screen render
+      target and then passing the rendered image as a texture to first filter
+      in the post-process chain. The image is then used as a source for rendering
+      for the next filter with a custom shader.
+
+      To create a custom post-processing filter the typical way is to derive from
+      this class and write a shader that performs the desired effect. To use
+      the custom shader you should override the filter() function, set the shader
+      and corresponding parameters to the style parameter and then call the
+      base implementation that does the actual work.
+  */
   class LUMINOUS_API PostProcessFilter : public Valuable::Node
   {
   public:
+    /// Creates a new post processing filter
     PostProcessFilter(Valuable::Node * host = 0, const QByteArray & name = "");
     virtual ~PostProcessFilter();
 
+    /// Called by the application when the scene is updated.
     virtual void update(float dt);
 
+    /// Initialize is called when a context is created for the filter. Override
+    /// this if you need to specify additional parameter to the context, such as
+    /// extra buffer attachments.
+    /// Note: this function needs to be thread-safe.
+    /// @param rc the current render context
+    /// @param ctx context for the render side post-processing
     virtual void initialize(Luminous::RenderContext & rc,
-                            Luminous::PostProcessContext & pc) const;
+                            Luminous::PostProcessContext & ctx) const;
 
+    /// Performs the filtering operation by rendering the scene using the
+    /// specified style.
+    /// Note: this function needs to be thread-safe.
+    /// @param rc the current render context
+    /// @param ctx context for the render side post-processing
+    /// @param style The style used for rendering ie. the filtering
     virtual void filter(Luminous::RenderContext & rc,
-                        Luminous::PostProcessContext & pc,
+                        Luminous::PostProcessContext & ctx,
                         Luminous::Style style = Luminous::Style()) const;
 
+    /// Checks if the filter is enabled, disabled filters will be skipped.
+    /// @return true if enabled, false otherwise.
     bool enabled() const;
+    /// Sets the enabled state of the filter.
+    /// @param enabled true to enable, false to disable.
     void setEnabled(bool enabled);
 
   private:
