5b362bf4e332c201d42e1c2008d9a69bd25dee5f
Riku Palomäki <riku@palomaki.fi>
2013-03-14 14:14:46 +0200

    Added CallStack::toStringList()

--- multitude/Radiant/CallStackLinux.cpp
+++ multitude/Radiant/CallStackLinux.cpp
@@ -74,9 +74,14 @@ namespace Radiant
   {
   }
 
-  void CallStack::print() const
+  QStringList CallStack::toStringList() const
   {
+    if (!m_cache.isEmpty())
+      return m_cache;
+
     char ** strings = backtrace_symbols(stack(), size());
+
+    QStringList tmp;
 
     /// strings[i] can be something like:
     /// /home/riku/cornerstone/multitude/lib/libRadiant.so.1(_ZN7Radiant8MemCheckC2Ev+0x40) [0x7fe3fd420c64]
@@ -102,14 +107,22 @@ namespace Radiant
           file = r.cap(1);
 
         if(func.isEmpty())
-          Radiant::error("#%-2d %p at %s", int(i), m_frames[i], file.toUtf8().data());
+          tmp << QString("#%1 0x%2 at %3").arg(i, -2).arg((intptr_t)m_frames[i], 0, 16).arg(file);
         else
-          Radiant::error("#%-2d %s at %s", int(i), func.data(), file.toUtf8().data());
+          tmp << QString("#%1 %2 at %3").arg(i, -2).arg(func.data(), file);
       } else {
-        Radiant::error("#%-2d %s", int(i), strings[i]);
+        tmp << QString("#%1 %3").arg(i, -2).arg(strings[i]);
       }
     }
     free (strings);
+    m_cache = tmp;
+    return tmp;
+  }
+
+  void CallStack::print() const
+  {
+    for (auto & str: toStringList())
+      Radiant::error("%s", str.toUtf8().data());
   }
 }
 
--- multitude/Radiant/CallStackW32.cpp
+++ multitude/Radiant/CallStackW32.cpp
@@ -145,9 +145,16 @@ namespace Radiant
   {
   }
 
-  void CallStack::print() const
+  QStringList CallStack::toStringList() const
   {
-      Radiant::Guard lock(s_dbgHelpMutex);
+    if (!m_cache.isEmpty())
+      return m_cache;
+
+    QStringList tmp;
+    Radiant::Guard lock(s_dbgHelpMutex);
+
+    if (!m_cache.isEmpty())
+      return m_cache;
 
     HANDLE handle = GetCurrentProcess();
     DWORD64 offset;
@@ -179,8 +186,17 @@ namespace Radiant
         if (GetLastError() != ERROR_INVALID_ADDRESS)
           Radiant::warning("Unable to get symbol line information: err %d\n", GetLastError());
       }
-      Radiant::info("#%d %s at %s:%d", i, pSymbol->Name, sym_line.FileName, sym_line.LineNumber);
+      tmp << QString("#%1 %3 at %4:%2").arg(i, -2).arg(sym_line.LineNumber).arg(pSymbol->Name, sym_line.FileName);
     }
+
+    m_cache = tmp;
+    return tmp;
+  }
+
+  void CallStack::print() const
+  {
+    for (auto & str: toStringList())
+      Radiant::error("%s", str.toUtf8().data());
   }
 }
 
