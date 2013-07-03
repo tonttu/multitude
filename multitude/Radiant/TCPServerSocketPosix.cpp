b903b241002c2eed594efb62acc1ced2fff35b21
Esa Nuuros <esa@multitouch.fi>
2013-07-03 15:04:36 +0300

    More useful error reports from TCPServerSocketPosix

--- multitude/Radiant/TCPServerSocketPosix.cpp
+++ multitude/Radiant/TCPServerSocketPosix.cpp
@@ -66,7 +66,7 @@ namespace Radiant
     int err = SocketUtilPosix::bindOrConnectSocket(fd, host, port, errstr,
                   true, AF_INET, SOCK_STREAM, IPPROTO_TCP);
     if(err) {
-      error("TCPServerSocket::open # %s", errstr.toUtf8().data());
+      error("TCPServerSocket::open(%s:%d) # %s", host, port, errstr.toUtf8().data());
       return err;
     }
 
