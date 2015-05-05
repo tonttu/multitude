23d9433be95013d9f03c405f8d87ecb8b83cf49d
Razvan Lar <razvan.lar@multitaction.com>
2015-05-05 15:20:54 +0300

    Improvements to Valuable::ListenerHolder
    (cherry picked from commit 902c9b9c6d50ae52c553f358e04848fcf653abfa)

--- multitude/Valuable/ListenerHolder.cpp
+++ multitude/Valuable/ListenerHolder.cpp
@@ -1,73 +1,89 @@
 #include "ListenerHolder.hpp"
+#include <utility>
 
 namespace Valuable
 {
+  ListenerHolder::ListenerHolder() {  }
+
+  ListenerHolder::ListenerHolder(ListenerHolder && other) noexcept
+    : m_deleteListeners(std::move(other.m_deleteListeners)),
+      m_attributeListeners(std::move(other.m_attributeListeners)),
+      m_eventListeners(std::move(other.m_eventListeners)) { }
+
+  ListenerHolder & ListenerHolder::operator=(ListenerHolder && other) noexcept
+  {
+    using std::swap;
+    if(&other == this) {
+      return *this;
+    }
+    swap(m_deleteListeners, other.m_deleteListeners);
+    swap(m_attributeListeners, other.m_attributeListeners);
+    swap(m_eventListeners, other.m_eventListeners);
+    return *this;
+  }
 
   ListenerHolder::~ListenerHolder()
   {
-    for (auto p: m_listeners)
-      p.first->removeListener(p.second);
-
-    for (auto p: m_eventListeners)
-      p.first->eventRemoveListener(p.second);
+    for(const auto & pair : m_deleteListeners) {
+      Attribute * attr = pair.first;
+      long id = pair.second;
+      attr->removeListener(id);
+    }
+    for(const auto & pair : m_attributeListeners) {
+      Attribute * attr = pair.first;
+      long id = pair.second;
+      attr->removeListener(id);
+    }
+    for(const auto & pair : m_eventListeners) {
+      Node * node = pair.first;
+      long id = pair.second;
+      node->eventRemoveListener(id);
+    }
   }
 
-
-  void ListenerHolder::add(Valuable::Attribute *attr, Valuable::Attribute::ListenerFunc func)
+  long ListenerHolder::add(Attribute * attr, const Attribute::ListenerFunc & func, int role)
   {
     assert(attr);
-    m_listeners.push_back(std::make_pair(attr, attr->addListener(func)));
-
-    setupAttributeRemoveListener(attr);
+    long id = attr->addListener(func, role);
+    m_attributeListeners.insert(std::make_pair(attr, id));
+    setupRemoveListener(attr);
+    return id;
   }
 
-  void ListenerHolder::add(Valuable::Node *node, const QByteArray &name, Valuable::Node::ListenerFuncVoid func)
+  long ListenerHolder::add(Node * node, const QByteArray & name, const Node::ListenerFuncVoid & func, Node::ListenerType listenerType)
   {
     assert(node);
-    m_eventListeners.push_back(std::make_pair(node, node->eventAddListener(name, func)));
-
-    setupNodeRemoveListener(node);
+    long id = node->eventAddListener(name, func, listenerType);
+    m_eventListeners.insert(std::make_pair(node, id));
+    setupRemoveListener(node);
+    return id;
   }
 
-  void ListenerHolder::add(Valuable::Node *fromNode, const QByteArray &from, const QByteArray &to, Valuable::Node *toNode)
+  long ListenerHolder::add(Node * node, const QByteArray & name, const Node::ListenerFuncBd & func, Node::ListenerType listenerType)
   {
-    assert(fromNode);
-    assert(toNode);
-    m_eventListeners.push_back(std::make_pair(fromNode, fromNode->eventAddListener(from, to, toNode)));
-
-    setupNodeRemoveListener(fromNode);
+    assert(node);
+    long id = node->eventAddListenerBd(name, func, listenerType);
+    m_eventListeners.insert(std::make_pair(node, id));
+    setupRemoveListener(node);
+    return id;
   }
 
-  void ListenerHolder::setupAttributeRemoveListener(Valuable::Attribute *attr)
+  void ListenerHolder::setupRemoveListener(Attribute * attr)
   {
-    auto rmFunc = [this, attr] {
-
-      auto it = std::remove_if(m_listeners.begin(), m_listeners.end(), [attr] (const std::pair<Valuable::Attribute*, long> & element)
-      {
-        return element.first == attr;
-      });
-
-      m_listeners.erase(it, m_listeners.end());
-    };
-
-    auto id = attr->addListener(rmFunc, Attribute::DELETE_ROLE);
-    m_listeners.push_back(std::make_pair(attr, id));
+    if(m_deleteListeners.find(attr) == m_deleteListeners.end()) {
+      long id = attr->addListener([attr, this] {
+        attributeGotDeleted(attr);
+      }, Attribute::DELETE_ROLE);
+      m_deleteListeners[attr] = id;
+    }
   }
 
-  void ListenerHolder::setupNodeRemoveListener(Valuable::Node *node)
+  void ListenerHolder::attributeGotDeleted(Attribute * attr)
   {
-    auto rmFunc = [this, node] {
-
-      auto it = std::remove_if(m_eventListeners.begin(), m_eventListeners.end(), [node] (const std::pair<Valuable::Node*, long>& element)
-      {
-        return element.first == node;
-      });
-
-      m_eventListeners.erase(it, m_eventListeners.end());
-    };
-
-    auto id = node->addListener(rmFunc, Attribute::DELETE_ROLE);
-    m_listeners.push_back(std::make_pair(node, id));
+    m_deleteListeners.erase(attr);
+    m_attributeListeners.erase(attr);
+    // Safe to downcast. If the attr is not a node, it will not be found in m_eventListeners and nothing
+    // will happen. If it is found, we know it's a Node.
+    m_eventListeners.erase(static_cast<Node*>(attr));
   }
-
 }
