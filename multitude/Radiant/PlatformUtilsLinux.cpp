abc666a9c91c418ccc0bff01a03cc251294fb996
Razvan Lar <razvan.lar@multitaction.com>
2015-11-26 15:38:24 +0200

    Replace system() with Radiant::FileUtils::runInShell()
    system() blocks SIGCHLD and ignores SIGINT, which can cause all sorts of
    issues. It's also a good idea to have a level of indirection so we can
    modify the implementation if we need to.
    
    See #9039

--- multitude/Radiant/PlatformUtilsLinux.cpp
+++ multitude/Radiant/PlatformUtilsLinux.cpp
@@ -14,6 +14,7 @@
 
 #include "PlatformUtils.hpp"
 #include "Trace.hpp"
+#include "FileUtils.hpp"
 
 #include <dlfcn.h>
 #include <assert.h>
@@ -32,26 +33,12 @@
 
 namespace
 {
-
   // Utility similar to system()
   int run(QString cmd, QStringList argv = QStringList(),
           QByteArray * out = 0, QByteArray * err = 0)
   {
-    QProcess p;
-    p.start(cmd, argv, QProcess::ReadOnly);
-    p.waitForStarted();
-    p.waitForFinished(-1);
-    if(out) *out = p.readAllStandardOutput();
-    if(err) {
-      *err = p.readAllStandardError();
-    } else {
-      QByteArray e = p.readAllStandardError();
-      if(!e.isEmpty())
-        Radiant::error("%s: %s", cmd.toUtf8().data(), e.data());
-    }
-    return p.exitCode();
+    return Radiant::FileUtils::run(cmd, argv, out, err);
   }
-
 }
 
 namespace Radiant
@@ -140,7 +127,7 @@ namespace Radiant
 
       const QString cmd = QString("grep %1 /proc/%2/maps | awk '{print $6}'| head -n1 > %3").arg(libraryName).arg(pid).arg(file.fileName());
 
-      if (system(cmd.toUtf8().data()) != 0)
+      if (FileUtils::runInShell(cmd) != 0)
         Radiant::error("PlatformUtils::getLibraryPath # Failed to get library path for %s", libraryName.toUtf8().data());
 
       return file.readAll().trimmed();
@@ -172,7 +159,7 @@ namespace Radiant
     {
       const auto cmd = QString("killall %1").arg(processName);
 
-      int err = system(cmd.toUtf8().data());
+      int err = FileUtils::runInShell(cmd);
       if(err != 0)
         Radiant::warning(QString("terminateProcessByName # failed to run '%1'").arg(cmd).toUtf8().data());
     }
