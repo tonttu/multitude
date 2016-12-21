44378d90cc22f2ff0cfe03c6b761a5178e9691a5
Pekka Mikkola <pekka@multitaction.com>
2016-12-21 12:24:41 +0200

    Remove redundant documentation. Refs #11403

--- multitude/Valuable/Attribute.hpp
+++ multitude/Valuable/Attribute.hpp
@@ -183,10 +183,6 @@ namespace Valuable
     @param name The name (or id) of this attribute. Names are typically
     human-readable. The names should not contain white-spaces
     as they may be used in XML files etc.
-
-    @param transit Should value changes be transmitted forward. This
-    is related to future uses, and can be largely ignored at the
-    moment.
     */
     Attribute(Node * host, const QByteArray & name);
     virtual ~Attribute();
