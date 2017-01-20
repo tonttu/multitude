fdbf78b26d739128ad701834a77b835b5efd759f
Riku Palomaki <riku@multitouch.fi>
2017-01-20 16:41:52 +0200

    Fixed issues noticed in PenEvent peer-checking, refs #11697

--- multitude/Radiant/PenEvent.hpp
+++ multitude/Radiant/PenEvent.hpp
@@ -46,20 +46,26 @@ namespace Radiant
 
     enum Type
     {
+      /// Invalid / null event
       TYPE_NONE,
+      /// Pen was pressed
       TYPE_DOWN,
+      /// Pen was released
       TYPE_UP,
+      /// Pen state update
       TYPE_UPDATE,
     };
 
   public:
     /// Default constructor, members are initilized by default values
-    PenEvent() = default;
+    inline PenEvent() = default;
 
     /// Initializes event based on corresponding
     /// @ref http://doc.qt.io/qt-5/qtabletevent.html "QTabletEvent".
     RADIANT_API PenEvent(QTabletEvent & event);
 
+    /// Pen id. With some devices and some platforms the ids might not be
+    /// unique and the ids could be reused.
     uint32_t id() const { return m_id; }
     void setId(uint32_t id) { m_id = id; }
 
@@ -70,19 +76,27 @@ namespace Radiant
     /// @param location New location to set
     void setLocation(Nimble::Vector2f location) { m_location = location; }
 
+    /// Event type
     Type type() const { return m_type; }
     void setType(Type type) { m_type = type; }
 
+    /// Flags tell the state of possible buttons and whether pressure and
+    /// other information is available
     Flags flags() const { return m_flags; }
     void setFlags(Flags flags) { m_flags = flags; }
 
+    /// Pen tip pressure from 0 (pen is barely touching the screen) to 1
+    /// (fully pressed). Only valid if FLAG_PRESSURE is set
     float pressure() const { return m_pressure; }
     void setPressure(float pressure) { m_pressure = pressure; }
 
+    /// Pen rotation from 0 to 2pi. Only valid if FLAG_ROTATION is set
     float rotation() const { return m_rotation; }
     void setRotation(float rotation) { m_rotation = rotation; }
 
-    // X and Y tilting angle between -pi/2 to pi/2
+    /// X and Y tilting angle between -pi/2 to pi/2. (0, 0) means that the
+    /// pen is perpendicular to the screen. Only valid if FLAG_TILT_* flags
+    /// are set
     Nimble::Vector2f tilt() const { return m_tilt; }
     void setTilt(Nimble::Vector2f tilt) { m_tilt = tilt; }
 
