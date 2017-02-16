bdc0b806b0028c145d165383f6f2a899a9c9cb32
Aki Reijonen <aki@multitouch.fi>
2017-02-16 14:43:23 +0200

    Fix missing default arguments in AttributeBool::as{Int,String}
    Related to #9395

--- multitude/Valuable/AttributeBool.hpp
+++ multitude/Valuable/AttributeBool.hpp
@@ -37,9 +37,9 @@ namespace Valuable
     virtual bool set(float v, Layer layer = USER, ValueUnit unit = VU_UNKNOWN) OVERRIDE;
     virtual bool set(const StyleValue & v, Layer layer = USER) OVERRIDE;
 
-    virtual float asFloat(bool * const ok = 0, Layer layer = CURRENT_VALUE) const OVERRIDE;
-    virtual int asInt(bool * const ok, Layer layer) const OVERRIDE;
-    virtual QString asString(bool * const ok, Layer layer) const OVERRIDE;
+    virtual float asFloat(bool * const ok = nullptr, Layer layer = CURRENT_VALUE) const OVERRIDE;
+    virtual int asInt(bool * const ok = nullptr, Layer layer = CURRENT_VALUE) const OVERRIDE;
+    virtual QString asString(bool * const ok = nullptr, Layer layer = CURRENT_VALUE) const OVERRIDE;
 
     static inline bool interpolate(bool a, bool b, float m)
     {
