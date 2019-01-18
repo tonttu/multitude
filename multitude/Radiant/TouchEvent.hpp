5b44ed4ae10e0f95c1aa8d493b9415f68d92f64b
Riku Palomäki <riku@multitouch.fi>
2019-01-18 15:35:16 +0200

    Added pressure, flags and raw location to pens in NetBridge protocol

--- multitude/Radiant/TouchEvent.hpp
+++ multitude/Radiant/TouchEvent.hpp
@@ -1,7 +1,7 @@
 #ifndef TOUCHEVENT_HPP
 #define TOUCHEVENT_HPP
 
-#include "Export.hpp"
+#include "PenEvent.hpp"
 
 #include <Nimble/Vector2.hpp>
 
@@ -24,16 +24,7 @@ namespace Radiant
       TOUCH_END ///< End of touch
     };
 
-    enum Flag
-    {
-      /// No flags
-      FLAG_NONE       = 0,
-
-      /// Event originates from a RawInputHandler. Event raw location uses
-      /// a relative units and has value of 0..1 instead of himetric units
-      FLAG_RAW        = 1 << 7,
-    };
-    typedef Radiant::FlagsT<Flag> Flags;
+    using RawLocationType = PenEvent::RawLocationType;
 
     /// Construct a new TouchEvent
     TouchEvent(int id = -1, Type type = TOUCH_BEGIN, Nimble::Vector2f location = {0, 0})
@@ -53,18 +44,20 @@ namespace Radiant
     /// @return Type of the event.
     Type type() const { return m_type; }
 
-    /// Event flags
-    Flags flags() const { return m_flags; }
-    void setFlags(Flags flags) { m_flags = flags; }
-
     /// Touch point location in desktop coordinates
     Nimble::Vector2f location() const { return m_location; }
     void setLocation(Nimble::Vector2f location) { m_location = location; }
 
-    /// Raw event location in device coordinates. In windows this is in
-    /// himetric units or relative units from 0..1 depending on FLAG_RAW
+    /// Raw event location in device coordinates. Only valid if FLAG_RAW_LOCATION
+    /// is set. Notice that this is in different units than location and
+    /// also different from PenData::rawLocation.
+    /// @sa rawLocationType
     Nimble::Vector2f rawLocation() const { return m_rawLocation; }
     void setRawLocation(Nimble::Vector2f location) { m_rawLocation = location; }
+
+    /// How to interpret rawLocation values.
+    RawLocationType rawLocationType() const { return m_rawLocationType; }
+    void setRawLocationType(RawLocationType rawLocationType) { m_rawLocationType = rawLocationType; }
 
     /// Unique ID for the source device. This can be typecasted to device
     /// HANDLE in Windows.
@@ -79,13 +72,12 @@ namespace Radiant
   private:
     int m_id;
     Type m_type;
-    Flags m_flags = FLAG_NONE;
+    RawLocationType m_rawLocationType = PenEvent::RAW_LOCATION_HIMETRIC;
     Nimble::Vector2f m_location;
     Nimble::Vector2f m_rawLocation{-1, -1};
     uint64_t m_sourceDevice = 0;
     double m_time = 0;
   };
-  MULTI_FLAGS(TouchEvent::Flag)
 
 }
 
