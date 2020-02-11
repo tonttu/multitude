9b0c64031c621857b4801facbfee6eb1444b801b
Riku Palomäki <riku@multitaction.com>
2020-02-11 10:27:05 +0200

    Added Radiant::FpsTracker

--- multitude/Radiant/TimeTracker.hpp
+++ multitude/Radiant/TimeTracker.hpp
@@ -37,8 +37,46 @@ namespace Radiant
     const char * m_func;
     const double m_threshold;
   };
+
+  /// Utility class for debugging performance. Don't use in production code.
+  ///
+  /// Example:
+  /// void myRandomFunction()
+  /// {
+  ///   // Print how often this function is called
+  ///   RADIANT_TRACK_FPS;
+  /// }
+  class FpsTracker
+  {
+  public:
+    FpsTracker(const char * filename, int lineno, const char * func)
+      : m_filename(filename)
+      , m_lineno(lineno)
+      , m_func(func)
+    {}
+
+    void update()
+    {
+      ++m_times;
+      if (m_timer.time() >= 1.0) {
+        double time = m_timer.start();
+        int times = m_times;
+        m_times = 0;
+        Radiant::info("%s:%d [%s]: %.3f fps", m_filename, m_lineno, m_func, times / time);
+      }
+    }
+
+  private:
+    Radiant::Timer m_timer;
+    int m_times = 0;
+    const char * m_filename;
+    const int m_lineno;
+    const char * m_func;
+  };
 }
 
 #define RADIANT_TRACK_TIME_CONCAT_(x, y) x ## y
 #define RADIANT_TRACK_TIME_CONCAT(x, y) RADIANT_TRACK_TIME_CONCAT_(x, y)
 #define RADIANT_TRACK_TIME(threshold) Radiant::TimeTracker RADIANT_TRACK_TIME_CONCAT(rtt, __LINE__)(__FILE__, __LINE__, __func__, (threshold))
+#define RADIANT_TRACK_FPS static Radiant::FpsTracker RADIANT_TRACK_TIME_CONCAT(rft, __LINE__)(__FILE__, __LINE__, __func__); \
+  RADIANT_TRACK_TIME_CONCAT(rft, __LINE__).update()
